43d0e100e8a3f9f85a49c5345862004a28b61e7e
DOMINGUES NUNES Jorge (JRC-EXT) <Jorge.DOMINGUES-NUNES@ext.ec.europa.eu>
2018-05-30 15:42:17 +0200

    Adding missing modules and converting CSS from blocks to code

--- sites/default/drushrc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$aliases['local'] = array(
-  'root' => 'C:\PGM\xampp\xampp\htdocs\jrccties',
-  'uri'  => 'localhost',
-  'path-aliases' => array(
-    '%dump-dir' => 'C:\PGM\xampp\xampp\tmp',
-  ),
-);
