a89afd7fe6cb83a51b71477d51ed2f86bd46cdfd
DOMINGUES NUNES Jorge (JRC-EXT) <Jorge.DOMINGUES-NUNES@ext.ec.europa.eu>
2018-05-30 15:37:44 +0200

    Adding missing modules and CSS fixes

---
+++ sites/default/drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+$aliases['local'] = array(
+  'root' => 'C:\PGM\xampp\xampp\htdocs\jrccties',
+  'uri'  => 'localhost',
+  'path-aliases' => array(
+    '%dump-dir' => 'C:\PGM\xampp\xampp\tmp',
+  ),
+);
