66d9b86c5081aaa3cc6c54ce6ef6f6aa12c903ad
VIDENOV Oleg Lachezarov (JRC-EXT) <Oleg-Lachezarov.VIDENOV@ext.ec.europa.eu>
2019-10-29 15:51:26 +0100

    JRCCTIES-51 Temporary commit

--- sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
+++ sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
@@ -1,7 +1,23 @@
-<?php foreach ($content_per_user as $c): ?>
-  <h2> <?php echo $c['title']; ?> </h2>
+
+Dear <?php echo $realname; ?>,
+
+Welcome to our monthly community newsletter.
+Your fellow community members create, discuss and share new comments, ideas, event information and news.
+We hope to keep you updated just in case you missed what happened last month in your community.
+
+Kindest regards,
+
+The JRC community support team
+
+You are receiving this newsletter because you previously ticked that box when becoming a member of the community.
+While you are welcome to unsubscribe through the link at the bottom of this email, we hope you continue to subscribe to our monthly newsletter.
+
+
+<?php foreach ($content_per_user as $community): ?>
+  <?php echo $community['logo']; ?>
+  <h2> <?php echo $community['title']; ?> </h2>
   <ul>
-  <?php foreach ($c['links'] as $a => $b): ?>
+  <?php foreach ($community['links'] as $a => $b): ?>
     <li> <?php echo $b; ?> </li>
   <?php endforeach; ?>
   </ul>
