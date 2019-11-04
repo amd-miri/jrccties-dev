d064f2a0104808b5787c261407ee0a3cafb594f8
VIDENOV Oleg Lachezarov (JRC-EXT) <Oleg-Lachezarov.VIDENOV@ext.ec.europa.eu>
2019-11-04 16:55:04 +0100

    JRCCTIES-51-newsletter-mail

--- sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
+++ sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
@@ -1,24 +1,52 @@
+<div class="newsletter-body">
+    <div class="newsletter-intro">
+        Dear <?php echo $realname; ?>,
+        Welcome to our monthly community newsletter.
+        Your fellow community members create, discuss and share new comments, ideas, event information and news.
+        We hope to keep you updated just in case you missed what happened last month in your community.
+        Kindest regards,
 
-Dear <?php echo $realname; ?>,
+        The JRC community support team
 
-Welcome to our monthly community newsletter.
-Your fellow community members create, discuss and share new comments, ideas, event information and news.
-We hope to keep you updated just in case you missed what happened last month in your community.
+        You are receiving this newsletter because you previously ticked that box when becoming a member of the community.
+        While you are welcome to unsubscribe through the link at the bottom of this email, we hope you continue to subscribe to our monthly newsletter.
+    </div>
+  <?php foreach ($content_per_user as $community): ?>
+    <div class="newsletter-community">
+        <div class="newsletter-community-left">
+            <div class="newsletter-community-logo">
+              <?php echo $community['logo']; ?>
+            </div>
+        </div>
 
-Kindest regards,
+        <div class="newsletter-community-right">
+            <div class="newsletter-community-title">
+                <h2> <?php echo $community['title']; ?> </h2>
+            </div>
 
-The JRC community support team
-
-You are receiving this newsletter because you previously ticked that box when becoming a member of the community.
-While you are welcome to unsubscribe through the link at the bottom of this email, we hope you continue to subscribe to our monthly newsletter.
-
-
-<?php foreach ($content_per_user as $community): ?>
-  <?php echo $community['logo']; ?>
-  <h2> <?php echo $community['title']; ?> </h2>
-  <ul>
-  <?php foreach ($community['links'] as $a => $b): ?>
-    <li> <?php echo $b; ?> </li>
+            <div class="newsletter-community-content">
+                <ul>
+                  <?php foreach ($community['links'] as $a => $b): ?>
+                      <li> <?php echo $b; ?> </li>
+                  <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
   <?php endforeach; ?>
-  </ul>
-<?php endforeach; ?>
+
+    <div class="newsletter-outro">
+        If you want to unsubscribe, please <?php echo $unsubscribe_link; ?>.
+    </div>
+</div>
+
+<table>
+    <tr>
+        <td></td>
+    </tr>
+</table>
+
+
+
+
+
