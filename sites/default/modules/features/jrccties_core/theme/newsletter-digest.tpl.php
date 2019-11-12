297ec3f05106dfed50af3ea2fd17bb2e0ce68c06
VIDENOV Oleg Lachezarov (JRC-EXT) <Oleg-Lachezarov.VIDENOV@ext.ec.europa.eu>
2019-11-12 18:17:02 +0100

    JRCCTIES-51 adding html template

--- sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
+++ sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
@@ -1,52 +1,117 @@
-<div class="newsletter-body">
-    <div class="newsletter-intro">
-        Dear <?php echo $realname; ?>,
-        Welcome to our monthly community newsletter.
-        Your fellow community members create, discuss and share new comments, ideas, event information and news.
-        We hope to keep you updated just in case you missed what happened last month in your community.
-        Kindest regards,
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+</head>
 
-        The JRC community support team
+<body bgcolor="#ffffff" style="background-color: #ffffff !important;">
 
-        You are receiving this newsletter because you previously ticked that box when becoming a member of the community.
-        While you are welcome to unsubscribe through the link at the bottom of this email, we hope you continue to subscribe to our monthly newsletter.
-    </div>
-  <?php foreach ($content_per_user as $community): ?>
-    <div class="newsletter-community">
-        <div class="newsletter-community-left">
-            <div class="newsletter-community-logo">
-              <?php echo $community['logo']; ?>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+<div>&nbsp;</div>
+
+<table cellpadding="0" cellspacing="0" height="100%" id="bodytable" style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" width="100%">
+  <tr>
+    <td align="center" bgcolor="#eaeaea" style="background-color: #ffffff; vertical-align: top" valign="top">
+      <table border="0" cellpadding="0" cellspacing="0" class="container" style="width: 650px" width="650">
+        <tr>
+          <td>
+            <div class="newsletter-intro">
+              <p><b>Dear <?php echo $realname; ?>,</b></p>
+              <p>
+                Welcome to our monthly community newsletter. Your fellow
+                community members create, discuss and share new comments, ideas,
+                event information and news.
+                We hope to keep you updated just in case you missed what
+                happened last month in your community.
+              </p>
+              <p> Kindest regards, </p>
+              <p>The JRC community support team</p>
             </div>
-        </div>
+            <div>&nbsp;</div>
+          </td>
+        </tr>
+      </table>
 
-        <div class="newsletter-community-right">
-            <div class="newsletter-community-title">
-                <h2> <?php echo $community['title']; ?> </h2>
-            </div>
+      <?php foreach ($content_per_user as $community): ?>
+      <table valign="top" border="1" bordercolor="#e3e1d9" cellpadding="0" cellspacing="0" class="container" style="border: 1px solid #eaeaea; width: 650px" width="650">
+        <tr>
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" style="width: 100%" width="100%">
+              <tr>
+                <td style="background-color: #ffffff;">
+                  <table class="table" border="0" cellpadding="0" cellspacing="0" style="width: 80%; background-color: #e3e1d9;" width="80%" align="right">
+                    <tr>
+                      <td class="cell" style="padding-left: 10px">
+                        <table border="0" cellpadding="0" cellspacing="0" style="width: 100% !important;" width="100%">
+                          <tr>
+                            <td style="height: 100px;">
+                              <h2> <?php echo $community['title']; ?>  </h2>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
 
-            <div class="newsletter-community-content">
-                <ul>
-                  <?php foreach ($community['links'] as $a => $b): ?>
-                      <li> <?php echo $b; ?> </li>
+                  <table class="table" border="0" valign="top" cellpadding="0" cellspacing="0" style="width: 20%; vertical-align: top; background-color: #e3e1d9;" width="20%" align="left";>
+                    <tr>
+                      <td valign="top" class="cell" style="padding-right: 10px; vertical-align:top;">
+                        <table border="0" cellpadding="0" cellspacing="0" valign="top" style="width: 100% !important" width="100%">
+                          <tr valign="top">
+                            <td style="padding: 0px 0px 0px 0px" valign="middle" align="center">
+                              <?php echo $community['logo']; ?>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
+
+                </td>
+              </tr>
+            </table>
+
+            <table cellpadding="0" cellspacing="0" width="80%" align="right">
+              <tr>
+                <td style="padding-top: 20px;">
+                  <?php foreach ($community['links'] as $key => $value): ?>
+                    <p> <?php echo $value; ?> </p>
                   <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
-    </div>
-  <?php endforeach; ?>
+                  <div>&nbsp;</div>
+                </td>
+              </tr>
+            </table>
 
-    <div class="newsletter-outro">
-        If you want to unsubscribe, please <?php echo $unsubscribe_link; ?>.
-    </div>
-</div>
+          </td>
 
-<table>
-    <tr>
-        <td></td>
-    </tr>
+        </tr>
+      </table>
+      <?php endforeach; ?>
+    </td>
+  </tr>
 </table>
 
+<table cellpadding="0" cellspacing="0" height="100%" id="bodytable" style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" width="100%">
+  <tr>
+    <td align="center" bgcolor="#ffffff" style="background-color: #ffffff; vertical-align: top" valign="top">
+      <table cellpadding="0" cellspacing="0" class="container" style="background: red !important; width: 650px" width="650">
+        <tr>
+          <td>
+            <div class="newsletter-footer">
+              <div>&nbsp;</div>
+              <p> You are receiving this newsletter because you previously ticked that box when becoming a member of the community. While you can unsubscribe, we hope you continue to subscribe to our monthly newsletter.
+              </p>
+              <div>&nbsp;</div>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
 
-
-
-
+</body>
+</html>
