b6845b2b120dffba6d6a041cf7cbb9b4f0f81d79
VIDENOV Oleg Lachezarov (JRC-EXT) <Oleg-Lachezarov.VIDENOV@ext.ec.europa.eu>
2020-01-29 10:55:31 +0100

    Synchronizing with reference repository.

--- sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
+++ sites/default/modules/features/jrccties_core/theme/newsletter-digest.tpl.php
@@ -16,31 +16,31 @@
 
 <body bgcolor="#ffffff" style="background-color: #ffffff !important;">
 
-  <table style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" cellpadding="0" cellspacing="0" height="100%" class="bodytable" width="100%">
-    <tr>
-      <td style="background-color: #ffffff;" align="center" bgcolor="#eaeaea">
-        <table style="width: 650px" border="0" cellpadding="0" cellspacing="0" class="container" width="650">
-          <tr>
-            <td>
-              <table cellpadding="0" cellspacing="0" border="0">
-                <tr>
-                  <td class="newsletter-text">
-                    <p style="margin: 10px;"> &nbsp; </p>
-                    <p style="margin:0;"><b>Dear <?php echo $realname; ?>,</b></p>
-                    <p style="margin-top: 10px;">
-                      Welcome to our monthly community newsletter. Your fellow community members create, discuss and share new comments, ideas, event information and news. We hope to keep you updated just in case you missed what happened last month in your community.
-                    </p>
-                    <p style="margin-top: 10px;"> Kindest regards, </p>
-                    <p style="margin-top: 10px;">The JRC community support team</p>
-                    <p style="margin: 5px;"> &nbsp; </p>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-        </table>
+<table style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" cellpadding="0" cellspacing="0" height="100%" class="bodytable" width="100%">
+  <tr>
+    <td style="background-color: #ffffff;" align="center" bgcolor="#eaeaea">
+      <table style="width: 650px" border="0" cellpadding="0" cellspacing="0" class="container" width="650">
+        <tr>
+          <td>
+            <table cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td class="newsletter-text">
+                  <p style="margin: 10px;"> &nbsp; </p>
+                  <p style="margin:0;"><b><?php echo t('Dear !realname', array('!realname' => $realname)); ?>,</b></p>
+                  <p style="margin-top: 10px;">
+                    <?php echo t('Welcome to our monthly community newsletter. Your fellow community members create, discuss and share new comments, ideas, event information and news. We hope to keep you updated just in case you missed what happened last month in your community.'); ?>
+                  </p>
+                  <p style="margin-top: 10px;"> <?php echo t('Kindest regards,'); ?> </p>
+                  <p style="margin-top: 10px;"> <?php echo t('The JRC community support team'); ?></p>
+                  <p style="margin: 5px;"> &nbsp; </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
 
-        <?php foreach ($content_per_user as $community): ?>
+      <?php foreach ($content_per_user as $community): ?>
         <table style="width: 650px" width="650" valign="top" cellpadding="0" cellspacing="0" class="container" border="1" bordercolor="#e3e3e3">
           <tr>
             <td>
@@ -91,30 +91,30 @@
             </td>
           </tr>
         </table>
-        <?php endforeach; ?>
-      </td>
-    </tr>
-  </table>
+      <?php endforeach; ?>
+    </td>
+  </tr>
+</table>
 
-  <table style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" cellpadding="0" cellspacing="0" height="100%" class="bodytable" width="100%">
-    <tr>
-      <td align="center" bgcolor="#ffffff" style="background-color: #ffffff; vertical-align: top" valign="top">
-        <table cellpadding="0" cellspacing="0" class="container" style="margin-top: 20px; width: 650px" width="650">
-          <tr>
-            <td>
-              <table cellpadding="0" cellspacing="0" border="0">
-                <tr>
-                  <td class="newsletter-text">
-                    <p style="margin-top: 30px;"> You are receiving this newsletter because you previously ticked that box when becoming a member of the community. While you can <?php echo $unsubscribe_link; ?>, we hope you continue to subscribe to our monthly newsletter. </p>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table>
+<table style="width: 100%; height: 100%; margin: 0px; padding: 0px; min-width: 320px" cellpadding="0" cellspacing="0" height="100%" class="bodytable" width="100%">
+  <tr>
+    <td align="center" bgcolor="#ffffff" style="background-color: #ffffff; vertical-align: top" valign="top">
+      <table cellpadding="0" cellspacing="0" class="container" style="margin-top: 20px; width: 650px" width="650">
+        <tr>
+          <td>
+            <table cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td class="newsletter-text">
+                  <p style="margin-top: 30px;"> <?php echo t('You are receiving this newsletter because you previously ticked that box when becoming a member of the community. While you can !unsubscribe_link, we hope you continue to subscribe to our monthly newsletter.', array('!unsubscribe_link' => $unsubscribe_link)); ?> </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
 
 </body>
 </html>
